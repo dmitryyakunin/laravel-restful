38ccb6eb151a35523e199da281cc56d3ea7df677
Dmitry Yakunin <yakunin_dmitry@irand.ru>
2021-03-30 15:51:50 +0300

    create categories

---
+++ resources/js/components/MainApp.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2> Интернет магазин </h2>
+        <router-view />
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "App"
+}
+
+</script>
+
+<style scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+Route::get('/', function () {
+    return view('my_welcome');
 });
 
 Auth::routes();
