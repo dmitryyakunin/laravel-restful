8ee87ec3fa0a055ff24bfd9c656ad04f93c195c6
yakunin_dmitry <yakunin_dmitry@irand.ru>
2021-04-01 00:25:33 +0300

    Search Products

---
+++ resources/js/components/SearchProducts.vue
@@ -0,0 +1,136 @@
+<template>
+    <div>
+        <h4> Поиск товаров </h4>
+        <div class="row">
+            <div class="col-sm-6">
+                <form>
+                    <div class="form-group">
+                        <label for="searchString">Строка поиска</label>
+                        <input type="text" class="form-control" id="searchString" placeholder="введите запрос"
+                               v-model="searchString">
+                    </div>
+                    <button class="btn btn-outline-primary" v-on:click="search()">Найти</button>
+                </form>
+            </div>
+            <div class="col-sm-6">
+                <div class="card" style="width: 30rem;">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" v-model="searchType" id="radios1"
+                                           value="option1" checked>
+                                    <label class="form-check-label" for="radios1">
+                                        имя товара
+                                    </label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" v-model="searchType" id="radios2"
+                                           value="option2">
+                                    <label class="form-check-label" for="radios2">
+                                        id категории
+                                    </label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" v-model="searchType" id="radios3"
+                                           value="option3">
+                                    <label class="form-check-label" for="radios3">
+                                        имя категории
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" v-model="searchType" id="radios4"
+                                           value="option4" checked>
+                                    <label class="form-check-label" for="radios4">
+                                        цена от - до
+                                    </label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" v-model="searchType" id="radios5"
+                                           value="option5">
+                                    <label class="form-check-label" for="radios5">
+                                        опубликованные
+                                    </label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" v-model="searchType" id="radios6"
+                                           value="option6">
+                                    <label class="form-check-label" for="radios6">
+                                        не удаленные
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <p></p>
+        <table class="table table-bordered">
+            <tr class="table-active">
+                <th> ID</th>
+                <th> Наименование</th>
+                <th> Описание</th>
+                <th> Цена</th>
+            </tr>
+            <tr v-for="item in products" :key="item.id">
+                <td> {{ item.id }}</td>
+                <td> {{ item.name }}</td>
+                <td> {{ item.description }}</td>
+                <td> {{ item.price }}</td>
+            </tr>
+        </table>
+        <br/>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Products",
+    data: () => ({
+        products: null,
+        searchType: 'option1',
+        searchString: null
+    }),
+    methods: {
+        getProducts() {
+            axios
+                .get('http://laravel-restful/api/products/')
+                .then(response => (this.products = response.data.data));
+        },
+        search() {
+            if (this.searchType === 'option1') {    // имя товара
+                let url = 'http://laravel-restful/api/product-name/' + this.searchString
+                axios
+                    .get(url)
+                    .then(response => (this.products = response.data.data));
+            }
+            if (this.searchType === 'option2') {    // id категории
+                let url = 'http://laravel-restful/api/product-category-id/' + this.searchString
+                axios
+                    .get(url)
+                    .then(response => (this.products = response.data.data));
+            }
+            if (this.searchType === 'option3') {    // Название категории
+                let url = 'http://laravel-restful/api/product-category-name/' + this.searchString
+                axios
+                    .get(url)
+                    .then(response => (this.products = response.data.data));
+            }
+        }
+    },
+    mounted() {
+        this.getProducts()
+    }
+}
+</script>
+
+<style scoped>
+.position {
+    margin-top: 10px;
+    margin-bottom: 5px;
+}
+</style>
