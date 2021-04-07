64a80c3bbe94e88bb6ad5964a9171dca22184e90
Dmitry Yakunin <yakunin_dmitry@irand.ru>
2021-04-07 15:54:39 +0300

    REST API CategoryResourceCollection, ProductResourceCollection with link

--- app/Http/Resources/CategoryResourceCollection.php
+++ app/Http/Resources/CategoryResourceCollection.php
@@ -14,6 +14,16 @@ class CategoryResourceCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        //return parent::toArray($request);
+        return ['data' => $this->collection];
+    }
+
+    public function with($request)
+    {
+        return [
+            'links'    => [
+                'self' => route('categories.index'),
+            ],
+        ];
     }
 }
